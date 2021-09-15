a9deb0203f52243e298326dd5a72abf3812b27d0
Muia Muteti <fstsmuia@gmail.com>
2021-09-15 17:27:27 +0300

    tailwind configuration: container,fonts,colors

---
+++ tailwind.config.js
@@ -0,0 +1,36 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        primary: "#2ecc71",
+        secondary: "#f18343",
+        "primary-txt": "#434343",
+        "secondary-txt": "#1f1f1f",
+        reward: "#39ba70",
+        "dark-bg": "#34495e",
+        "refer-txt": "#b79e56",
+        "refer-bg": "#f8edce" 
+      }
+    },
+    fontFamily: {
+      'primary': ['Montserrat', 'Roboto', 'sans-serif']
+    },
+    container: {
+      center: true,
+      padding: "1rem",
+      screens: {
+        sm: "640px",
+        md: "768px",
+        lg: "1024px",
+        xl: "1200px",
+        "2xl": "1200px"
+      }
+    }
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
